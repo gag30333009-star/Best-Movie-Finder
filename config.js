a0585dae44787472ee2fb30ebaebb08ff454f642
Gag <gag30333009@gmail.com>
2026-05-31 00:37:27 +0400

    TT

---
+++ config.js
@@ -0,0 +1,5 @@
+// 🔒 PRIVATE - Do not share or push to GitHub
+// This file contains your personal API key
+// Get your key at: https://www.themoviedb.org/settings/api
+
+const API_KEY = "9ce0cf2826f0da553eedb79720c1f58f";
